a10a5784a958956c707984d0cea60c1c7bca611b
wzxinchen <1101385391@qq.com>
2020-01-07 18:26:44 +0800

    初始

---
+++ src/Blazui.Docs.Admin.EFCore/DocsDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Blazui.Docs.Admin.EFCore
+{
+    public class DocsDbContext : IdentityDbContext
+    {
+        public DocsDbContext()
+        {
+
+        }
+        public DocsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+            {
+                return;
+            }
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.UseNpgsql("Host=127.0.0.1;Port=5432;Username=postgres;Password=12345678;Database=blazui_docs");
+        }
+    }
+}
---
+++ src/Blazui.Docs.Admin/Shared/MainLayoutBase.cs
@@ -0,0 +1,30 @@
+﻿using BlazAdmin;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Docs.Admin.Shared
+{
+    public class MainLayoutBase : LayoutComponentBase
+    {
+        protected List<MenuModel> Menus { get; set; } = new List<MenuModel>();
+
+        protected override void OnInitialized()
+        {
+            Menus.Add(new MenuModel()
+            {
+                Label = "产品管理",
+                Icon = "el-icon-star-on",
+                Route = "products"
+            });
+            Menus.Add(new MenuModel()
+            {
+                Label = "文档管理",
+                Icon = "el-icon-document",
+                Route = "docs"
+            });
+        }
+    }
+}
